143cd08bc66a965f41d1139ad8e8762861b72695
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-02-21 16:40:09 +0900

    스프링부트 프로젝트 개발환경 생성

---
+++ niniAI-backEnd/src/main/kotlin/com/niniAI/backEnd/niniAI_backEnd/NiniAiBackEndApplication.kt
@@ -0,0 +1,11 @@
+package com.niniAI.backEnd.niniAI_backEnd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NiniAiBackEndApplication
+
+fun main(args: Array<String>) {
+	runApplication<NiniAiBackEndApplication>(*args)
+}
---
+++ niniAI-backEnd/src/test/kotlin/com/niniAI/backEnd/niniAI_backEnd/NiniAiBackEndApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.niniAI.backEnd.niniAI_backEnd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NiniAiBackEndApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
